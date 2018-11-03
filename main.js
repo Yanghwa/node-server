6e6bd6e0818d459f300429447ff13f9ad8b7857c
Yanghwa <kellerj87@hotmail.com>
2018-11-03 11:04:54 -0400

    error handler set for 404, 500

--- main.js
+++ main.js
@@ -67,4 +67,13 @@ app.post('/authors/delete', (req, res) => {
     author.delete_process(req, res);
 });
 
+app.use((req, res, next) => {
+    res.status(404).send('Sorry cant find that!');
+});
+   
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
+
 app.listen(3000);
