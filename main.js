6a1b9a8ea1cf7890b2989d3244d78c65c00ab8da
Yanghwa <kellerj87@hotmail.com>
2018-11-03 00:10:28 -0400

    unused node path detect removed

--- main.js
+++ main.js
@@ -42,41 +42,4 @@ app.post('/delete', (req, res) => {
     topic.delete_process(req, res);
 });
 
-// const app = http.createServer((req, res) => {
-//     const _url = req.url;
-//     const queryData = url.parse(_url, true).query;
-//     const pathname = url.parse(_url, true).pathname;
-//     let title = queryData.id;
-//     if(pathname === '/') {
-//         if(title === undefined) {
-//             topic.home(req, res);
-//         } else {
-//             topic.page(req, res);
-//         }
-//     } else if(pathname == '/create') {
-//         topic.create(req, res);
-//     } else if(pathname === '/create_process'){
-//         topic.create_process(req, res);
-//     } else if(pathname === '/update'){
-//         topic.update(req, res);
-//     } else if(pathname === '/update_process'){
-//         topic.update_process(req, res);
-//     } else if(pathname === '/delete_process'){
-//         topic.delete_process(req, res);
-//     } else if(pathname === '/authors'){
-//         author.home(req, res);
-//     } else if(pathname === '/authors/create_process'){
-//         author.create_process(req, res);
-//     } else if(pathname === '/authors/update') {
-//         author.update(req, res);
-//     } else if(pathname === '/authors/update_process') {
-//         author.update_process(req, res);
-//     } else if(pathname === '/authors/delete_process'){
-//         author.delete_process(req, res);
-//     } else {
-//         res.writeHead(404);
-//         res.end('Not Found');
-//     }
-// });
-
 app.listen(3000);
