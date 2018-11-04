7ee556a142a19a630aae4d3f21fa3ffc0e15dfac
Yanghwa <kellerj87@hotmail.com>
2018-11-04 17:47:39 -0500

    separate passport

--- main.js
+++ main.js
@@ -38,52 +38,15 @@ app.get('/flash-display', function(req, res){
     res.send(fmsg);
 });
 
-let tempAuthData = {
-    email: 'testing@gmail.com',
-    password: '1111',
-    nickname: 'Super Test'
-}
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-    done(null, user.email);
-});
-
-passport.deserializeUser((id, done) => {
-    done(null, tempAuthData);
-});
-
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-}, (username, password, done) => {
-    if(username === tempAuthData.email) {
-        if(password === tempAuthData.password) {
-            return done(null, tempAuthData, {
-                message: 'Welcome.'
-            });
-        } else {
-            return done(null, false, {
-                message: 'Incorrect password.'
-            });
-        }
-    } else {
-        return done(null, false, {
-            message: 'Incorrect username.'
-        });
-    }
-}));
+const passport = require('./lib/passport')(app);
 app.post('/auth/login',
     passport.authenticate('local', {
         successRedirect: '/',
         failureRedirect: '/auth/login',
         failureFlash:true,
         successFlash:true
-    }));
+}));
 
 //custom middleware
 app.get('*', middleware.list);
